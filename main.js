cd735795f8e2d0786c14d4ba05134484bf8cbe1f
neversuptou <danilasereda2@yandex.ru>
2023-10-04 07:29:42 +0300

    try quote

--- main.js
+++ main.js
@@ -134,3 +134,26 @@ if(LS.getItem('savedName')) {
 // saveBtn.addEventListener("click", function() {
 //   localStorage.setItem("savedInput", input.value);
 // });
+
+// QUOTE
+const quote = document.querySelector('.quote');
+const author = document.querySelector('.author');
+const quoteBtn = document.querySelector('.change-quote')
+
+async function getQuote() {
+    const quotes = 'https://neversuptou.github.io/js-pet/data.json'
+    const res = await fetch(quotes);
+    const data = await res.json();
+
+    const numberOfQuote = Math.floor(Math.random() * data.length)
+
+    quote.textContent = data[numberOfQuote].text
+    author.textContent = data[numberOfQuote].author
+}
+
+function changeQuote() {
+    quoteBtn.addEventListener('click', () => {
+        getQuote()
+    })
+}
+changeQuote()
